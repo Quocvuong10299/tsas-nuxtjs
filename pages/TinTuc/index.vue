0521ec0e49439671f42fd7bb61f1ad9aa718035e
phongbui241299 <phongbui241299@gmail.com>
2021-01-07 16:26:36 +0700

    dev: style TinTuc page

--- pages/TinTuc/index.vue
+++ pages/TinTuc/index.vue
@@ -1,10 +1,433 @@
 <template>
   <div class="wrapper">
-    <h1>This is News Page</h1>
+    <div class="header__banner  mb70">
+      <div class="image-banner-top pt_35percent pl135 bg_1 position-relative">
+        <div class="ellipse">
+          <h2 class="max-w200">
+            Tin tức
+          </h2>
+        </div>
+      </div>
+    </div>
+    <div class="w-partner mb70">
+      <div class="row mb30px">
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-1 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-2 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-3 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-4 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-5 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-6 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-7 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-8 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-9 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-10 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-11 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-12 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-13 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-14 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-15 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div class="d-flex col-lg-3 col-md-3 cl-12 mb50">
+          <div class="mg-auto">
+            <div class="w-banner-new news-banner-16 image-cover position-relative mb15" />
+            <div class="w-banner-new">
+              <h4 class="font-weight-500 pb5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit tortor ac
+              </h4>
+              <h5 class="font-weight-300 text-color_5 pb10">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Rutrum dictum mattis odio amet fusce.
+              </h5>
+              <h5 class="text-red">
+                John Eve • 01/11/2019
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--      <div class='d-flex'>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number bg-color_5 border-gray"-->
+      <!--        >-->
+      <!--          <div-->
+      <!--            class="icon mb30px"-->
+      <!--            v-html="icOne"-->
+      <!--          />-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          type="submit"-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number border-gray"-->
+      <!--        >-->
+      <!--          1-->
+      <!--        </a>-->
+      <!--        <a-->
+      <!--          href="#"-->
+      <!--          class="btn btn-primary custom-responsive d-block text-center mg-auto w-number bg-color_5 border-gray"-->
+      <!--        >-->
+      <!--          <div-->
+      <!--            class="icon mb30px"-->
+      <!--            v-html="icTwo"-->
+      <!--          />-->
+      <!--        </a>-->
+      <!--      </div>-->
+    </div>
   </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {};
+  },
+  computed: {
+    icOne(){
+      const icOne = require("assets/images/ic-prev.svg?raw");
+      return icOne;
+    },
+    icTwo(){
+      const icOne = require("assets/images/ic-next.svg?raw");
+      return icOne;
+    },
+  }
 };
 </script>
+<style scoped>
+.bg_1 {
+  background-image: url("~assets/images/bg_5.jpg");
+}
+.border-gray{
+  border: 1px solid #989898 !important;
+}
+.w-number{
+  width: 60px!important;
+  height: 60px!important;
+}
+
+.w-banner-new{
+  width: 270px;
+  height: 180px;
+}
+
+.news-banner-1{
+  background-image: url("~assets/images/news-banner-1.png");
+}
+
+.news-banner-1{
+  background-image: url("~assets/images/news-banner-1.png");
+}
+
+.news-banner-2{
+  background-image: url("~assets/images/news-banner-2.png");
+}
+
+.news-banner-3{
+  background-image: url("~assets/images/news-banner-3.png");
+}
+
+.news-banner-4{
+  background-image: url("~assets/images/news-banner-4.png");
+}
+
+.news-banner-5{
+  background-image: url("~assets/images/news-banner-5.png");
+}
+
+.news-banner-6{
+  background-image: url("~assets/images/news-banner-6.png");
+}
+
+.news-banner-7{
+  background-image: url("~assets/images/news-banner-7.png");
+}
+
+.news-banner-8{
+  background-image: url("~assets/images/news-banner-8.png");
+}
+
+.news-banner-9{
+  background-image: url("~assets/images/news-banner-9.png");
+}
+
+.news-banner-10{
+  background-image: url("~assets/images/news-banner-10.png");
+}
+
+.news-banner-11{
+  background-image: url("~assets/images/news-banner-11.png");
+}
+
+.news-banner-12{
+  background-image: url("~assets/images/news-banner-12.png");
+}
+
+.news-banner-13{
+  background-image: url("~assets/images/news-banner-13.png");
+}
+
+.news-banner-14{
+  background-image: url("~assets/images/news-banner-14.png");
+}
+
+.news-banner-15{
+  background-image: url("~assets/images/news-banner-15.png");
+}
+
+.news-banner-16{
+  background-image: url("~assets/images/news-banner-16.png");
+}
+
+</style>
